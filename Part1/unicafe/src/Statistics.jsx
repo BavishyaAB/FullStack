577121ed338234d2c49ab6403846f2c8a815877f
Bavishya <bavishya1399@gmail.com>
2025-08-15 14:35:34 +0200

    Exercise 1.9

--- Part1/unicafe/src/Statistics.jsx
+++ Part1/unicafe/src/Statistics.jsx
@@ -2,6 +2,14 @@ const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good - bad) / total || 0;
   const positivePercentage = (good / total) * 100 || 0;
+  if (total === 0) {
+        return (
+        <>
+            <h1>Statistics</h1>
+            <p>No feedback given</p>
+        </>
+    );
+  }
   return (
     <>
       <h1>Statistics</h1>
