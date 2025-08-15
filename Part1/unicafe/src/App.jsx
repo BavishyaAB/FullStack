4e642110115bb71c80d673368af09a3638ac2254
Bavishya <bavishya1399@gmail.com>
2025-08-15 14:27:46 +0200

    Exercise 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -14,6 +14,9 @@ function App() {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {good + neutral + bad}</p>
+      <p>Average {(good - bad) / (good + neutral + bad) || 0}</p>
+      <p>Positive {good / (good + neutral + bad) * 100 || 0} %</p>
     </>
   )
 }
