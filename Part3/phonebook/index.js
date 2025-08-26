42bdcdbbda573ea462d1d192f703996bd907451e
BavishyaAB <bavishya1399@gmail.com>
2025-08-26 11:53:34 +0200

    Exercise 3.5 & 3.6

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const MAX_ID = 10000;
 const persons = [
     { 
       "id": "1",
@@ -23,6 +24,11 @@ const persons = [
       "number": "39-23-6423122"
     }
 ]
+const generateId = () => {
+    const min = persons.length + 1;
+    return (Math.floor(Math.random() * (MAX_ID-min))+min);
+}
+app.use(express.json());
 app.get('/', (req, res) => {    console.log('PhoneBook App')});
 app.get('/info', (req, res) => {
   const timestamp = new Date();
@@ -41,6 +47,39 @@ app.get('/api/persons/:id',(req,res)=>{
         res.status(404).send({ error: 'Person not found' });
     }
 });
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).send({ error: 'ID is required' });
+    }
+    const personIndex = persons.findIndex(p => p.id === id);
+    if (personIndex !== -1) {
+        persons.splice(personIndex, 1);
+        console.log(persons);
+        res.status(204).json(persons);
+    } else {
+        res.status(404).send({ error: 'Person not found' });
+    }
+});
+app.post('/api/persons', (req, res) => {
+    console.log(req.body);
+    const { name, number } = req.body;
+    if (!name || !number) {
+        return res.status(400).send({ error: 'Name and number are required' });
+    }
+    if (persons.find(p => p.name === name)) {
+        console.log('Name must be unique');
+        return res.status(400).send({ error: 'Name must be unique' });
+    }
+    const newPerson = {
+        id: String(generateId()),
+        name,
+        number
+    };
+    console.log(newPerson);
+    persons.push(newPerson);
+    res.status(201).json(newPerson);
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
