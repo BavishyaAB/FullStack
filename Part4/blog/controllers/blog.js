1a0461cc2335d91ee744e5816514852e90a53e81
BavishyaAB <bavishya1399@gmail.com>
2025-09-04 17:59:31 +0200

    Exercise 4.1 & 4.2

---
+++ Part4/blog/controllers/blog.js
@@ -0,0 +1,19 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/',(req, res, next) => {
+  Blog.find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
+    .catch(error => next(error))
+})
+blogsRouter.post('/',(req, res, next) => {
+  const newBlog = new Blog(req.body)
+  newBlog.save()
+    .then(savedBlog => {
+      res.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+module.exports = blogsRouter
